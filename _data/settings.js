cbc30412c65fa88236a2943fb8a496f15ca78800
Dan Ryan (GitHub Noreply Key) <dryan@users.noreply.github.com>
2024-04-11 18:04:31 -0400

    ♻️ upgrade to sanity 3; add pages and site settings

--- _data/settings.js
+++ _data/settings.js
@@ -10,16 +10,18 @@ const inspect = (obj) =>
 
 export default async () => {
   const settings = await sanity.site_settings({});
-  settings.footer_links = await Promise.all(
-    settings.footer_links.map(async (link) => {
-      if (link.page?._type === "reference") {
-        const page = await client.fetch(`*[_id == $id]{title, slug}[0]`, {
-          id: link.page._ref,
-        });
-        link.page = page;
-      }
-      return link;
-    })
-  );
+  if (settings.footer_links) {
+    settings.footer_links = await Promise.all(
+      settings.footer_links.map(async (link) => {
+        if (link.page?._type === "reference") {
+          const page = await client.fetch(`*[_id == $id]{title, slug}[0]`, {
+            id: link.page._ref,
+          });
+          link.page = page;
+        }
+        return link;
+      })
+    );
+  }
   return settings;
 };
